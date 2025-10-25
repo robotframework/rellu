2bce867c0d69214d16e6fa582c740c3cdc193080
Tatu Aalto <aalto.tatu@gmail.com>
2025-10-25 19:10:27 +0300

    chore: ruff format

--- rellu/releasenotes.py
+++ rellu/releasenotes.py
@@ -24,15 +24,16 @@ from invoke import Exit
 
 from .repo import get_repository
 
+
 class ReleaseNotesGenerator:
-    pre_intro = '''
+    pre_intro = """
 .. default-role:: code
-'''
-    post_intro = '''
+"""
+    post_intro = """
 .. contents::
    :depth: 2
    :local:
-'''.strip()
+""".strip()
 
     def __init__(self, repository, title, intro, default_targets=()):
         self.repository = repository
@@ -43,8 +44,9 @@ class ReleaseNotesGenerator:
 
     def generate(self, version, username=None, password=None, file=sys.stdout):
         if version.dev:
-            raise Exit(f'Cannot create release notes for development '
-                       f"version '{version}'.")
+            raise Exit(
+                f"Cannot create release notes for development version '{version}'."
+            )
         issues = self._get_issues(version, username, password)
         with self._output_enabled(file, version):
             self._write_intro(version)
@@ -62,22 +64,24 @@ class ReleaseNotesGenerator:
 
     def _get_issues_in_milestone(self, repository, version):
         milestone = self._get_milestone(repository, version)
-        for data in repository.get_issues(milestone=milestone, state='all'):
+        for data in repository.get_issues(milestone=milestone, state="all"):
             issue = Issue(data, repository.full_name)
             if issue.included_in_release_notes(version):
                 yield issue
 
     def _get_milestone(self, repository, version):
-        for milestone in repository.get_milestones(state='all'):
+        for milestone in repository.get_milestones(state="all"):
             if milestone.title == version.milestone:
                 return milestone
-        raise Exit(f"Milestone '{version.milestone}' not found from "
-                   f"repository '{repository.full_name}'.")
+        raise Exit(
+            f"Milestone '{version.milestone}' not found from "
+            f"repository '{repository.full_name}'."
+        )
 
     @contextmanager
     def _output_enabled(self, file, version):
         if isinstance(file, (str, Path)):
-            self._output = open(str(file).format(version=version), 'w')
+            self._output = open(str(file).format(version=version), "w")
             close = True
         else:
             self._output = file
@@ -92,35 +96,39 @@ class ReleaseNotesGenerator:
 
     def _write_intro(self, version):
         self._write_header(self.title.format(version=version), level=1)
-        intro = self.intro.format(version=version,
-                                  date=time.strftime('%A %B %-d, %Y'))
+        intro = self.intro.format(version=version, date=time.strftime("%A %B %-d, %Y"))
         self._write(self.pre_intro)
         self._write(intro, newlines=2)
         self._write(self.post_intro)
 
     def _write_most_important_enhancements(self, issues, version):
-        self._write_issues_with_label('Most important enhancements',
-                                      issues, version,
-                                      'priority: critical', 'priority: high')
+        self._write_issues_with_label(
+            "Most important enhancements",
+            issues,
+            version,
+            "priority: critical",
+            "priority: high",
+        )
 
     def _write_backwards_incompatible_changes(self, issues, version):
-        self._write_issues_with_label('Backwards incompatible changes',
-                                      issues, version,
-                                      'backwards incompatible')
+        self._write_issues_with_label(
+            "Backwards incompatible changes", issues, version, "backwards incompatible"
+        )
 
     def _write_deprecated_features(self, issues, version):
-        self._write_issues_with_label('Deprecated features',
-                                      issues, version,
-                                      'deprecation')
+        self._write_issues_with_label(
+            "Deprecated features", issues, version, "deprecation"
+        )
 
     def _write_acknowledgements(self, issues, version):
-        self._write_issues_with_label('Acknowledgements',
-                                      issues, version,
-                                      'acknowledge')
+        self._write_issues_with_label(
+            "Acknowledgements", issues, version, "acknowledge"
+        )
 
     def _write_issue_table(self, issues, version):
-        self._write_header('Full list of fixes and enhancements')
-        self._write('''
+        self._write_header("Full list of fixes and enhancements")
+        self._write(
+            """
 .. list-table::
     :header-rows: 1
 
@@ -128,86 +136,93 @@ class ReleaseNotesGenerator:
       - Type
       - Priority
       - Summary
-'''.strip())
-        prefix1 = '    * - '
-        prefix2 = '      - '
+""".strip()
+        )
+        prefix1 = "    * - "
+        prefix2 = "      - "
         if version.preview:
-            self._write('      - Added')
+            self._write("      - Added")
         for issue in issues:
             self._write(prefix1 + issue.id)
             self._write(prefix2 + issue.type)
             self._write(prefix2 + issue.priority)
             self._write(prefix2 + issue.summary)
             if version.preview:
-                self._write(prefix2 + issue.preview.replace(' ', u'\xa0'))
+                self._write(prefix2 + issue.preview.replace(" ", "\xa0"))
         self._write()
-        self._write('Altogether {} issue{}. View on the `issue tracker '
-                    '<https://github.com/{}/issues?q=milestone%3A{}>`__.'
-                    .format(len(issues), 's' if len(issues) != 1 else '',
-                            self.repository, version.milestone))
+        self._write(
+            "Altogether {} issue{}. View on the `issue tracker "
+            "<https://github.com/{}/issues?q=milestone%3A{}>`__.".format(
+                len(issues),
+                "s" if len(issues) != 1 else "",
+                self.repository,
+                version.milestone,
+            )
+        )
 
     def _write_targets(self, issues):
         self._write()
         for target in self.default_targets:
             self._write(target)
         for issue in issues:
-            self._write(f'.. _{issue.id}: {issue.url}', link_issues=False)
+            self._write(f".. _{issue.id}: {issue.url}", link_issues=False)
 
     def _write_header(self, header, level=2):
         if level > 1:
             self._write()
-        underline = {1: '=', 2: '=', 3: '-', 4: '~'}[level] * len(header)
+        underline = {1: "=", 2: "=", 3: "-", 4: "~"}[level] * len(header)
         if level == 1:
             self._write(underline)
         self._write(header)
         self._write(underline, newlines=2)
 
     def _write_issues_with_label(self, header, issues, version, *labels):
-        issues = [issue for issue in issues
-                  if any(label in issue.labels for label in labels)]
+        issues = [
+            issue for issue in issues if any(label in issue.labels for label in labels)
+        ]
         if not issues:
             return
         self._write_header(header)
-        self._write('**EXPLAIN** or remove these.', newlines=2)
+        self._write("**EXPLAIN** or remove these.", newlines=2)
         for issue in issues:
-            self._write(f'- {issue.summary} ({issue.id}', newlines=0)
+            self._write(f"- {issue.summary} ({issue.id}", newlines=0)
             if version.preview and issue.preview:
-                self._write(f', {issue.preview})')
+                self._write(f", {issue.preview})")
             else:
-                self._write(')')
+                self._write(")")
 
-    def _write(self, message='', newlines=1, link_issues=True):
-        message += '\n' * newlines
+    def _write(self, message="", newlines=1, link_issues=True):
+        message += "\n" * newlines
         if link_issues:
-            message = re.sub(r'(#\d+)', r'`\1`_', message)
+            message = re.sub(r"(#\d+)", r"`\1`_", message)
         self._output.write(message)
 
 
 @total_ordering
 class Issue:
-    NOT_SET = '---'
-    PRIORITIES = ['critical', 'high', 'medium', 'low', NOT_SET]
-    TYPES = ['bug', 'enhancement', 'task', NOT_SET]
+    NOT_SET = "---"
+    PRIORITIES = ["critical", "high", "medium", "low", NOT_SET]
+    TYPES = ["bug", "enhancement", "task", NOT_SET]
     GITHUB_TYPES = {
         "Bug": "bug",
         "Feature": "enhancement",
         "Task": "task",
-        NOT_SET: NOT_SET
+        NOT_SET: NOT_SET,
     }
 
     def __init__(self, issue: GitHubIssue, repository: str):
-        self.id = f'#{issue.number}'
+        self.id = f"#{issue.number}"
         self.milestone = issue.milestone.title
         # Avoid escaping problems with zero-width space in cases like `\`.
-        self.summary = issue.title.replace('\\`', '\\\N{ZERO WIDTH SPACE}`')
+        self.summary = issue.title.replace("\\`", "\\\N{ZERO WIDTH SPACE}`")
         self.labels = [label.name for label in issue.get_labels()]
-        self.url = f'https://github.com/{repository}/issues/{issue.number}'
+        self.url = f"https://github.com/{repository}/issues/{issue.number}"
         self.gh_issue_type = issue.type
 
     @property
     def preview(self):
         for label in self.labels:
-            if label.startswith(('alpha ', 'beta ', 'rc ')):
+            if label.startswith(("alpha ", "beta ", "rc ")):
                 return label
         return None
 
@@ -223,22 +238,24 @@ class Issue:
 
     @property
     def priority(self):
-        priorities = ['priority: ' + prio for prio in self.PRIORITIES]
+        priorities = ["priority: " + prio for prio in self.PRIORITIES]
         for label in self.labels:
             if label in priorities:
-                return label.split(': ')[1]
+                return label.split(": ")[1]
         return self.NOT_SET
 
     def included_in_release_notes(self, version):
-        if self.type == 'task':
+        if self.type == "task":
             return False
         return version.is_included(self)
 
     @property
     def sort_key(self):
-        return (self.PRIORITIES.index(self.priority),
-                self.TYPES.index(self.type),
-                self.id)
+        return (
+            self.PRIORITIES.index(self.priority),
+            self.TYPES.index(self.type),
+            self.id,
+        )
 
     def __eq__(self, other):
         return isinstance(other, Issue) and self.id == other.id
@@ -247,4 +264,4 @@ class Issue:
         return self.sort_key < other.sort_key
 
     def __str__(self):
-        return f'{self.id} {self.summary}'
+        return f"{self.id} {self.summary}"
--- utest/test_version.py
+++ utest/test_version.py
@@ -6,76 +6,77 @@ from rellu.version import Version
 
 
 def test_version():
-    for release in ('1.0', '10.9.2017'):
-        for preview in (None, 'a1', 'b2', 'rc3'):
-            version = release + (preview or '')
+    for release in ("1.0", "10.9.2017"):
+        for preview in (None, "a1", "b2", "rc3"):
+            version = release + (preview or "")
             v = Version(version)
             assert v.version == version
             assert v.release == release
-            assert v.milestone == 'v' + release
+            assert v.milestone == "v" + release
             assert v.preview == preview
             assert v.dev is None
-            v = Version(version + '.dev1')
-            assert v.version == version + '.dev1'
+            v = Version(version + ".dev1")
+            assert v.version == version + ".dev1"
             assert v.release == release
-            assert v.milestone == 'v' + release
+            assert v.milestone == "v" + release
             assert v.preview == preview
-            assert v.dev == '.dev1'
+            assert v.dev == ".dev1"
 
 
 def test_invalid_version():
-    for invalid in ('invalid', '1', '1.x', '1.0beta1', '1.0b'):
+    for invalid in ("invalid", "1", "1.x", "1.0beta1", "1.0b"):
         with pytest.raises(Exit):
             Version(invalid)
 
 
 def test_to_dev():
-    v = Version('1.0').to_dev()
-    assert v.version == '1.0.1.dev1'
-    assert v.release == '1.0.1'
+    v = Version("1.0").to_dev()
+    assert v.version == "1.0.1.dev1"
+    assert v.release == "1.0.1"
     assert v.preview is None
-    assert v.dev == '.dev1'
-    v = Version('1.0a1').to_dev()
-    assert v.version == '1.0a2.dev1'
-    assert v.release == '1.0'
-    assert v.preview == 'a2'
-    assert v.dev == '.dev1'
-    v = Version('1.0a1.dev1').to_dev()
-    assert v.version == '1.0a1.dev2'
-    assert v.release == '1.0'
-    assert v.preview == 'a1'
-    assert v.dev == '.dev2'
+    assert v.dev == ".dev1"
+    v = Version("1.0a1").to_dev()
+    assert v.version == "1.0a2.dev1"
+    assert v.release == "1.0"
+    assert v.preview == "a2"
+    assert v.dev == ".dev1"
+    v = Version("1.0a1.dev1").to_dev()
+    assert v.version == "1.0a1.dev2"
+    assert v.release == "1.0"
+    assert v.preview == "a1"
+    assert v.dev == ".dev2"
 
 
 def test_is_included():
-    for version, data in [('1.0a1', [('alpha 1', True),
-                                     ('alpha 2', False),
-                                     ('beta 1', False),
-                                     ('rc 2', False)]),
-                          ('1.0a2', [('alpha 1', True),
-                                     ('alpha 2', True),
-                                     ('beta 1', False),
-                                     ('rc 2', False)]),
-                          ('1.0b2', [('alpha 1', True),
-                                     ('alpha 2', True),
-                                     ('beta 1', True),
-                                     ('rc 2', False)]),
-                          ('1.0rc2', [('alpha 1', True),
-                                      ('alpha 2', True),
-                                      ('beta 1', True),
-                                      ('rc 2', True)]),
-                          ('1.0', [('alpha 1', True),
-                                   ('alpha 2', True),
-                                   ('beta 1', True),
-                                   ('rc 2', True)])]:
+    for version, data in [
+        (
+            "1.0a1",
+            [("alpha 1", True), ("alpha 2", False), ("beta 1", False), ("rc 2", False)],
+        ),
+        (
+            "1.0a2",
+            [("alpha 1", True), ("alpha 2", True), ("beta 1", False), ("rc 2", False)],
+        ),
+        (
+            "1.0b2",
+            [("alpha 1", True), ("alpha 2", True), ("beta 1", True), ("rc 2", False)],
+        ),
+        (
+            "1.0rc2",
+            [("alpha 1", True), ("alpha 2", True), ("beta 1", True), ("rc 2", True)],
+        ),
+        (
+            "1.0",
+            [("alpha 1", True), ("alpha 2", True), ("beta 1", True), ("rc 2", True)],
+        ),
+    ]:
         is_included = Version(version).is_included
         for preview, expected in data:
-            assert is_included(IssueStub('v1.0', preview)) is expected
-        assert is_included(IssueStub('v1.0', None)) is (version == '1.0')
+            assert is_included(IssueStub("v1.0", preview)) is expected
+        assert is_included(IssueStub("v1.0", None)) is (version == "1.0")
 
 
 class IssueStub:
-
     def __init__(self, milestone, preview):
         self.milestone = milestone
         self.preview = preview
